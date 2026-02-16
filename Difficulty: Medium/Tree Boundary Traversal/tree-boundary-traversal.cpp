6f8bcd275e24d3751e3431dc40537baa32a19ffb
IshankKumar31 <152838357+IshankKumar31@users.noreply.github.com>
2026-02-16 16:13:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
@@ -0,0 +1,76 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+  
+    void traverseLeft(Node* root, vector<int> &ans){
+        if(root==NULL ||(root->left==NULL && root->right == NULL)){
+            return;
+        }
+        ans.push_back(root->data);
+        if(root->left != NULL){
+            traverseLeft(root->left,ans);
+        }else{
+            traverseLeft(root->right,ans);
+        }
+    }
+    
+    void traverseRight(Node* root, vector<int> &ans){
+        if(root==NULL ||(root->left==NULL && root->right == NULL)){
+            return;
+        }
+        
+        if(root->right != NULL){
+            traverseRight(root->right,ans);
+        }else{
+            traverseRight(root->left,ans);
+        }
+        ans.push_back(root->data);
+    }
+    
+    void traverseLeaf(Node* root, vector<int> &ans){
+        if(root==NULL){
+            return;
+        }
+        if(root->left == NULL && root->right ==NULL){
+            ans.push_back(root->data);
+            return;
+        }
+        traverseLeaf(root->left,ans);
+        traverseLeaf(root->right,ans);
+    }
+    
+    vector<int> boundaryTraversal(Node *root) {
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+        }
+        
+        ans.push_back(root->data);
+        
+        traverseLeft(root->left,ans);
+        
+        traverseLeaf(root->left,ans);
+        traverseLeaf(root->right,ans);
+        
+        traverseRight(root->right,ans);
+        
+        return ans;
+        
+        
+    }
+};
